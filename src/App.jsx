82037245e8608b6b77690a5fe0fca7778cc0542d
danny-englander <dannyenglander311@gmail.com>
2023-03-28 11:09:46 -0400

    code cleanup and comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import BearsData from "./components/BearsData";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className='bears-app'>
       <BearsData />
--- vite.config.js
+++ vite.config.js
@@ -6,24 +6,29 @@ import fse from "fs-extra";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    // Call React.
     react(),
     {
+      // Custom function to copy and rename the built js file.
+      // That way, Drupal can always target the same filename in
+      // its library yaml file in the theme.
       name: "copy-and-rename-plugin",
       apply: "build",
       async writeBundle(outputOptions, bundle) {
-        // find the built JS file
+        // Glob the built JS file.
         const files = glob.sync(`${outputOptions.dir}/assets/*.js`);
         const originalPath = files[0];
         const newPath = `${outputOptions.dir}/assets/renamed-build.js`;
-        // copy the built JS file to the new path
+        // Copy the built JS file to the new path.
         await fse.copy(originalPath, newPath);
-        // rename the copied JS file
+        // Rename the copied JS file.
         await fse.rename(newPath, `${outputOptions.dir}/assets/bears.js`);
       },
     },
   ],
   build: {
     sourcemap: false,
-    outDir: "dist", // Set the output directory here
+    // Set the output directory here.
+    outDir: "dist",
   },
 });
